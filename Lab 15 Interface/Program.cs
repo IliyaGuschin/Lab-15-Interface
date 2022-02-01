425379cfda6ad69b28769962f4d1451e78267648
Илья <iliya.guschin@bk.ru>
2022-02-01 19:21:55 +0300

    Добавьте файлы проекта.

---
+++ Lab 15 Interface/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Lab_15_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    interface ISeries
+    {
+        void setStart(int x);
+        void getNext();
+        void reset();
+    }
+    class ArithProgression : ISeries
+    {
+        public void getNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setStart(int x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class GeomProgression : ISeries
+         
+    {
+        int pValue, pCurrentValue;
+
+        public void getNext()
+        {
+            return p;
+        }
+
+        public void reset()
+        {
+            pValue=0;
+        }
+
+        public void setStart(int x)
+        {
+        pValue = x;
+        }
+    }
+}
+
+
+
